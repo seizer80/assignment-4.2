908ae25f71227f5183de8d07dc08f4c71a3e463c
kangmin <seizer80@gmail.com>
2023-05-30 15:33:04 -0700

    firstone

---
+++ lab-answers.js
@@ -0,0 +1,11 @@
+// for (var input = 1; input <= 20; input++) {
+//     console.log(input);
+// }
+// Easy Going answered
+
+// for (i = 0; i <= 200; i += 2) {
+//     console.log(i);
+// }
+// "Get Even answered"
+
+
