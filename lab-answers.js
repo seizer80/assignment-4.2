7393dd330d9f1e994e2e0a0a115ee57e0acc5e7c
kangmin <seizer80@gmail.com>
2023-05-30 20:22:32 -0700

    second one

--- lab-answers.js
+++ lab-answers.js
@@ -8,4 +8,64 @@
 // }
 // "Get Even answered"
 
+// for (i = 1; i < 100; i++) {
+//     if (i % 3) {
+//         console.log("Fizz");
+//     } else if (i % 5) {
+//         console.log("Buzz");
+//     } else {
+//         console.log("FizzBuzz")
+//     }
+// }
+// "Fizz Buzz answered"
+
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant", 5000, "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
+// plantee[2] = 5001;
+// wolfy[3] = 'Gotham City';
+// dart.push("Hawkins");
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
+//                "Wild Wild Life answered"
+
+// const names = ["Donatello", "Leonardo", " Raphael", "Michaelangelo"]
+// for (const element of names) {
+//     const upperCased = element.toUpperCase();
+//     console.log(upperCased);
+// }
+// "Yell at the Ninja Turtles answered"
+
+
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// console.log(favMovies) = "Titanic"
+// favMovies.sort();
+// favMovies.pop();
+// favMovies.push("a few good man");
+// favMovies.reverse();
+// favMovies.shift();
+// favMovies.unshift();
+// favMovies.splice(3, 1, "Django Unchained", "Avatar");
+// favMovies.slice(3, 7);
+
+// console.log(favMovies);
+// "Methods Revisited answered"
+
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+// ["Lucinda", "Jacc", "Neff", "Snoop"],
+// ["Petunia", ["Baked Goods", "Waldo"]]];
+// // whereIsWaldo.splice(1, 1);
+// whereIsWaldo.splice(2, 2, "No one");
+// console.log(whereIsWaldo);
+// "Where is Waldo answered"
+
+let meow = 0;
+kittyTalk[meow]
+// "Excited Kittens answered"
+
+const nums = [14, 11, 16, 15, 13, 16, 15, 17, 19, 11, 12, 14, 19, 11, 15, 17, 11, 18, 12, 17, 12, 71, 18, 15, 12];
+// Find the Median answered"
 
